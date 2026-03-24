e8016201c35d2021b8f4922db4e17596a362932d
Karan <karanganesan0313@gmail.com>
2026-03-24 11:44:30 +0530

    Rename table from 'user' to 'users'

--- erp-backend/src/main/java/com/karan/model/User.java
+++ erp-backend/src/main/java/com/karan/model/User.java
@@ -1,5 +1,4 @@
 package com.karan.model;
-
 import jakarta.persistence.*;
 import lombok.*;
 import java.util.Set;
@@ -9,23 +8,16 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "user")
-
+@Table(name = "users")
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String username;
-
     @Column(unique = true)
     private String email;
-
     private String password;
-
     private boolean enable=true;
-
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_roles",
